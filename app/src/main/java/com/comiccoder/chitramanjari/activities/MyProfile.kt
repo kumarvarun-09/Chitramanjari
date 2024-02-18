6f96363a051983925c3e96587ce58fb80cf14726
Varun Kumar <varunkr2109@gmail.com>
2024-02-18 22:09:31 +0530

    my profile section appbar added

--- app/src/main/java/com/comiccoder/chitramanjari/activities/MyProfile.kt
+++ app/src/main/java/com/comiccoder/chitramanjari/activities/MyProfile.kt
@@ -2,6 +2,7 @@ package com.comiccoder.chitramanjari.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.comiccoder.chitramanjari.R
 import com.comiccoder.chitramanjari.adapters.ViewPagerAdapter
 import com.comiccoder.chitramanjari.constants.MY_POSTS_FRAGMENT_TITLE
@@ -13,11 +14,24 @@ import com.comiccoder.chitramanjari.fragments.MyReelsFragment
 import com.squareup.picasso.Picasso
 
 class MyProfile : AppCompatActivity() {
-    val binding by lazy{ActivityMyProfileBinding.inflate(layoutInflater)}
+    val binding by lazy { ActivityMyProfileBinding.inflate(layoutInflater) }
     private lateinit var viewPagerAdapter: ViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.materialToolbar)
+        val actionBar = supportActionBar
+
+        if (actionBar != null) {
+
+            // Customize the back button
+            actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back__black_24);
+
+            // showing the back button in action bar
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         getCurrentUserData {
             binding.name.text = it!!.name
             binding.email.text = it.email
@@ -30,5 +44,17 @@ class MyProfile : AppCompatActivity() {
         viewPagerAdapter.addFragment(MyReelsFragment(), MY_REELS_FRAGMENT_TITLE)
         binding.viewPager.adapter = viewPagerAdapter
         binding.tabLayout.setupWithViewPager(binding.viewPager)
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
